519a9338c5b6801159e03eaddca55a512040600a
elliospizzaman <elliospizzaman@users.noreply.github.com>
2013-10-05 17:53:07 -0400

    Update 2005-1.py

--- 2005-1.py
+++ 2005-1.py
@@ -1,14 +1,1 @@
-from sys import argv
-script, arg1, arg2 = argv
-arg1 = int(arg1)
-arg2 = int(arg2)
-print "Input: %d, %d" %(arg1, arg2)
-output = 0
-def add(x):
-	global output
-	output+=x
-if arg1<arg2:
-	map(add, range(arg1, arg2+1))
-else:
-	map(add, range(arg2, arg1+1))
-print "Output:", output
+from sys import stdin;data = sorted(map(int, stdin.readline().split()));print sum(range(data[0], data[1]+1))
